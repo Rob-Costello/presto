f7c44deceb5ee243d5b325d10fd2563ad219fbbb
Paul Winter <paul@stickymedia.co>
2018-04-16 09:56:37 +0100

    Updated text on press jobs views.

--- presto/views/pages/jobs.php
+++ presto/views/pages/jobs.php
@@ -2,12 +2,12 @@
     <div class="container-fluid">
         <div class="row bg-title">
             <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                <h4 class="page-title">Orders</h4>
+                <h4 class="page-title">Press Jobs</h4>
             </div>
             <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                 <ol class="breadcrumb">
                     <li><a href="#">Dashboard</a></li>
-                    <li class="active">Press Jpbs</li>
+                    <li class="active">Press Jobs</li>
                 </ol>
             </div>
             <!-- /.col-lg-12 -->
